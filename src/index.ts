2999c9d50a03df03fb8a4ab3ba429017dca18a04
namiranda <49685356+namiranda@users.noreply.github.com>
2021-02-20 16:15:17 -0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hola p");
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/request-validation-error';
+
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
